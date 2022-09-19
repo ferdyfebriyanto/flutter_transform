469385a5e3a27dcab858b64171d44e3cbb9c5ab6
ferdyfebriyanto <ferdyfebriyanto22@gmail.com>
2022-09-19 23:16:06 +0700

    Update home screen

---
+++ lib/home.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_transform/flip_widget.dart';
+import 'package:flutter_transform/perspective_widget.dart';
+import 'package:flutter_transform/transform_widget.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Manipulation Widget Ferdy'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                child: const Text('Transform Widget'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const TransformWidget(),
+                    ),
+                  );
+                },
+              ),
+              ElevatedButton(
+                child: const Text('Perspective Widget'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const PerspectiveWidget(),
+                    ),
+                  );
+                },
+              ),
+              ElevatedButton(
+                child: const Text('Flip'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => FlipWidget(
+                        child: Text('1'),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
